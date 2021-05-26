b781655631fb6ce4ba17292eba06b130bf94a621
Kilson <kilsonrs@gmail.com>
2021-05-26 09:46:23 -0300

    chore: add global theme

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { Container, Title } from './styles';
+
+const Dashboard: React.FC = () => {
+  return <Container><Title>Dashboard</Title></Container>;
+}
+
+export default Dashboard;
